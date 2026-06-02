0cfbdf4c193fa029252c55a83570e1b2e11ab5a6
YusuF Faisal <yusufoesta7t@gmail.com>
2026-06-02 20:36:21 +0600

    fix: zoom in favicon logo

--- app/icon.tsx
+++ app/icon.tsx
@@ -6,10 +6,12 @@ export const contentType = "image/png";
 export default function Icon() {
   return new ImageResponse(
     (
-      <img
-        src="https://ik.imagekit.io/2lax2ytm2/Screenshot%202026-05-30%20at%203.58.44%E2%80%AFPM.png"
-        style={{ width: 32, height: 32, borderRadius: 7 }}
-      />
+      <div style={{ width: 32, height: 32, borderRadius: 7, overflow: "hidden", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <img
+          src="https://ik.imagekit.io/2lax2ytm2/Screenshot%202026-05-30%20at%203.58.44%E2%80%AFPM.png"
+          style={{ width: 42, height: 42, objectFit: "cover" }}
+        />
+      </div>
     ),
     { ...size }
   );
